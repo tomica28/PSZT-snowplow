fca3da44d8e956dd25d5552fbe7fc612a82a54a6
Van Khanh Do <khanhdovanit@gmail.com>
2019-11-26 23:16:10 +0100

    Build states graph and optimize the cost

--- snowplow.py
+++ snowplow.py
@@ -1,27 +1,55 @@
 import networkx as nx
+import random
 
-
-def is_edge_in_path(edge, path):
-    for i in range(len(path) - 1):
-        if(path[i] == edge[0] and path[i+1] == edge[1]):
+def is_edge_in_GoE(edge, goe):
+    for i in range(len(goe) - 1):
+        if(goe[i] == edge[0] and goe[i + 1] == edge[1]):
             return True
-        if(path[i] == edge[1] and path[i+1 == edge[0]]):
+        if(goe[i] == edge[1] and goe[i + 1 == edge[0]]):
             return True
     return False
-def sumPathWeight(path, graph):
+
+def total_weight_of_GoE(goe, graph):
     weight = 0
-    for i in range(len(path)-1):
-        weight += graph.get_edge_data(path[i], path[i+1])['weight']
+    for i in range(len(goe) - 1):
+        weight += graph.get_edge_data(goe[i], goe[i + 1])['weight']
     return weight
-def delRedundancy(pathList):
+
+def del_redundancy(list_of_goe):
     newList = []
-    for i in pathList:
+    for i in list_of_goe:
         reversedI = list.copy(i)
         reversedI.reverse()
         if (reversedI in newList or i in newList):
             continue
         newList.append(i)
     return newList
+
+# get all edges of group of edges - GoE
+def get_all_edges_of_GoE(goe):
+    edge_list = list()
+    for i in range(len(goe) - 1):
+        tup = (goe[i], goe[i+1])
+        edge_list.append(tup)
+    return edge_list
+
+# def get_group_of_GoE_with_no_common_edge(goe_list):
+#
+#     if len(goe_list) < 2 : return  goe_list
+#
+#     list_of_goe_group = list();
+#     index = 0
+#     for i in range(len(goe_list) - 2):
+#         edge_list = get_all_edges_of_GoE(goe_list[i])
+#         for j in range(i+1, len(goe_list) - 1):
+#             is_exist = False
+#             for e in edge_list:
+#                 if is_edge_in_GoE(e, goe_list[j]):
+#                     is_exist = True
+#                     break
+#             if not is_exist:
+#                 list_of_goe_group.append()
+
 
 #example graph for testing
 G = nx.Graph()
@@ -33,57 +61,69 @@ base = 'A'
 G.add_nodes_from(nodes)
 G.add_weighted_edges_from(edges)
 
-pathList = []
-
+list_of_goe = []
 
 for i in G.edges:
-    pathList.append(list(i))
+    list_of_goe.append(list(i))
 
-for i in pathList:
+for i in list_of_goe:
     nb1 = dict(G[i[0]])
     nb2 = dict(G[i[-1]])
-    pathWeight = sumPathWeight(i, G)
+    goe_weight = total_weight_of_GoE(i, G)
     for k in nb1.keys():
-        if is_edge_in_path([i[0], k], i):
+        if is_edge_in_GoE([i[0], k], i):
             continue
-        if(pathWeight + nb1[k]['weight'] <= snowPlow):
+        if(goe_weight + nb1[k]['weight'] <= snowPlow):
             temp = list.copy(i)
             temp.insert(0, k)
-            pathList.append(temp)
+            list_of_goe.append(temp)
     for k in nb2.keys():
-        if is_edge_in_path([i[-1], k], i):
+        if is_edge_in_GoE([i[-1], k], i):
             continue
-        if (pathWeight + nb2[k]['weight'] <= snowPlow):
+        if (goe_weight + nb2[k]['weight'] <= snowPlow):
             temp = list.copy(i)
             temp.append(k)
-            pathList.append(temp)
+            list_of_goe.append(temp)
 
-pathList = delRedundancy(pathList)
-# print(pathList)
-# print(len(pathList))
+list_of_goe = del_redundancy(list_of_goe)
+# print(list_of_goe)
+# print(len(list_of_goe))
 
-# pathDict = {}
-# for i in pathList:
-#     pathDict[tuple(i)] = (sumPathWeight(i, G), nx.shortest_path_length(G, base, i[0], 'weight'), nx.shortest_path_length(G, i[-1], base, 'weight'))
+# goe_dict = {}
+# for i in list_of_goe:
+#     goe_dict[tuple(i)] = (total_weight_of_GoE(i, G), nx.shortest_path_length(G, base, i[0], 'weight'), nx.shortest_path_length(G, i[-1], base, 'weight'))
 
-# print(pathDict)
+# print(goe_dict)
 
 # we will create states graph here
 # reverse the the list of groups of edges
-goe_list = pathList;
+goe_list = list_of_goe;
 goe_list.reverse();
-# print(goe_list)
+#print(goe_list)
 
-# build dictionary with keys are groups of edges, and values are total costs, include:
+# build dictionary with keys are groups of edges (goe), and values are total costs, include:
 # 1. base -> start point +
 # 2. the sum of length of edges +
 # 3. end point -> base
 goe_dict = {};
 for goe in goe_list:
-    goe_dict[tuple(goe)] = (sumPathWeight(goe, G) + nx.shortest_path_length(G, base, goe[0], 'weight') +
-                          nx.shortest_path_length(G, goe[-1], base, 'weight'))
+    goe_dict[tuple(goe)] = (total_weight_of_GoE(goe, G) + nx.shortest_path_length(G, base, goe[0], 'weight') +
+                            nx.shortest_path_length(G, goe[-1], base, 'weight'))
 
 print(goe_dict)
+
+# build the graph by looping all goe_dict dictionary
+graph_of_states = nx.Graph()
+graph_of_states.add_node(0)
+
+# for state in goe_dict:
+#     graph_of_states.add_node(state, weight = goe_dict[state])
+
+
+
+#graph_of_states.edges()
+
+print(graph_of_states)
 
 
 
