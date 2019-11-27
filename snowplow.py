fabef138fcbe78e953e28644888168cafd566190
Van Khanh Do <khanhdovanit@gmail.com>
2019-11-27 13:24:22 +0100

    Easy approach to optimize, almost done!

--- snowplow.py
+++ snowplow.py
@@ -1,11 +1,12 @@
 import networkx as nx
 import random
+import collections
 
 def is_edge_in_GoE(edge, goe):
     for i in range(len(goe) - 1):
         if(goe[i] == edge[0] and goe[i + 1] == edge[1]):
             return True
-        if(goe[i] == edge[1] and goe[i + 1 == edge[0]]):
+        if(goe[i] == edge[1] and goe[i + 1] == edge[0]):
             return True
     return False
 
@@ -33,29 +34,97 @@ def get_all_edges_of_GoE(goe):
         edge_list.append(tup)
     return edge_list
 
-# def get_group_of_GoE_with_no_common_edge(goe_list):
-#
-#     if len(goe_list) < 2 : return  goe_list
-#
-#     list_of_goe_group = list();
-#     index = 0
-#     for i in range(len(goe_list) - 2):
-#         edge_list = get_all_edges_of_GoE(goe_list[i])
-#         for j in range(i+1, len(goe_list) - 1):
-#             is_exist = False
-#             for e in edge_list:
-#                 if is_edge_in_GoE(e, goe_list[j]):
-#                     is_exist = True
-#                     break
-#             if not is_exist:
-#                 list_of_goe_group.append()
+def get_list_of_GoE_with_no_common_edge_with_firstGoE(goe_dict):
+
+    # dictionary and accumulation for output
+    out_dict = {}
+    acc_edge_list = {}
+    if len(goe_dict) == 0:
+        return [out_dict, acc_edge_list]
+
+    # get first element
+    first_key = list(goe_dict.keys())[0]
+    out_dict[first_key] = goe_dict[first_key]
+    # put edge list of the first GoE into accumulation edge list
+    acc_edge_list = get_all_edges_of_GoE(first_key)
+
+    if len(goe_dict) == 1:
+        return [out_dict, acc_edge_list]
+
+    for j in range(1, len(goe_dict)):
+        is_exist = False
+        for e in acc_edge_list:
+            if is_edge_in_GoE(e, list(goe_dict.keys())[j]):
+                is_exist = True
+                break
+        if not is_exist:
+            out_dict[list(goe_dict.keys())[j]] = list(goe_dict.values())[j]
+            acc_edge_list.extend(get_all_edges_of_GoE(list(goe_dict.keys())[j]))
+
+    return [out_dict, acc_edge_list]
+
+def get_weight_orderd_goe_list(goe_dict):
+    max_nodes = len(list(goe_dict.keys())[0])
+    weight_orderd_goe_list = list()
+    for i in range(max_nodes, 1, -1):
+        goe_with_the_same_number_of_nodes = {}
+        for key in goe_dict:
+            if len(list(key)) == i:
+                goe_with_the_same_number_of_nodes[key] = goe_dict[key]
+
+        weight_orderd_goe_list.append(goe_with_the_same_number_of_nodes)
+
+    # sort each dictionary in the list
+    for i in range(len(weight_orderd_goe_list)):
+        weight_orderd_goe_list[i] = dict(sorted(weight_orderd_goe_list[i].items(), key=lambda kv: kv[1]))
+
+    return weight_orderd_goe_list
+
+def remove_all_existed_edges(acc_edge_list, goe_dict):
+
+    if len(acc_edge_list) == 0:
+        return goe_dict
+
+    out_dict = {}
+    for j in range(len(goe_dict)):
+        is_exist = False
+        for e in acc_edge_list:
+            if is_edge_in_GoE(e, list(goe_dict.keys())[j]):
+                is_exist = True
+                break
+        if not is_exist:
+            out_dict[list(goe_dict.keys())[j]] = list(goe_dict.values())[j]
+    return out_dict
+
+def get_final_optimal_path(ordered_dict_list):
+    acc_edges_list_final = list()
+    optimal_path_dict_final = {}
+    for i in range(len(ordered_dict_list)):
+        # get dictionary at current stage
+        dict = ordered_dict_list[i]
+
+        # remove all GoEs at the one-step-lower level if having any common edge
+        # with existing edges of the optimal path
+        cleaned_dict = remove_all_existed_edges(acc_edges_list_final, dict)
+
+        # get optimal path and accumulation at this stage
+        if len(cleaned_dict) > 0:
+            acc_edges_list = list()
+            optimal_path_dict = {}
+            [optimal_path_dict, acc_edges_list] = get_list_of_GoE_with_no_common_edge_with_firstGoE(cleaned_dict)
+            # add optimal path and accumulation of this stage to
+            optimal_path_dict_final.update(optimal_path_dict)
+            acc_edges_list_final.extend(acc_edges_list)
+    return optimal_path_dict_final
 
 
 #example graph for testing
 G = nx.Graph()
 
 nodes = ['A', 'B', 'C', 'D', 'E', 'F']
-edges = [('A', 'B', 1), ('B', 'C', 2), ('C', 'D', 4), ('A', 'C', 3), ('A', 'F', 2), ('D', 'F', 3), ('D', 'E', 2), ('E', 'F', 3)]
+#edges = [('A', 'B', 1), ('B', 'C', 2), ('C', 'D', 4), ('A', 'C', 3), ('A', 'F', 2), ('D', 'F', 3), ('D', 'E', 2), ('E', 'F', 3)]
+edges = [('A', 'B', 5), ('B', 'C', 1), ('C', 'D', 2), ('A', 'C', 2),
+         ('A', 'F', 2), ('D', 'F', 2), ('D', 'E', 1), ('E', 'F', 1)]
 snowPlow = 6
 base = 'A'
 G.add_nodes_from(nodes)
@@ -86,14 +155,14 @@ for i in list_of_goe:
             list_of_goe.append(temp)
 
 list_of_goe = del_redundancy(list_of_goe)
-# print(list_of_goe)
+#print(list_of_goe)
 # print(len(list_of_goe))
 
-# goe_dict = {}
-# for i in list_of_goe:
-#     goe_dict[tuple(i)] = (total_weight_of_GoE(i, G), nx.shortest_path_length(G, base, i[0], 'weight'), nx.shortest_path_length(G, i[-1], base, 'weight'))
+goe_dict = {}
+for i in list_of_goe:
+    goe_dict[tuple(i)] = (total_weight_of_GoE(i, G), nx.shortest_path_length(G, base, i[0], 'weight'), nx.shortest_path_length(G, i[-1], base, 'weight'))
 
-# print(goe_dict)
+print(goe_dict)
 
 # we will create states graph here
 # reverse the the list of groups of edges
@@ -110,20 +179,25 @@ for goe in goe_list:
     goe_dict[tuple(goe)] = (total_weight_of_GoE(goe, G) + nx.shortest_path_length(G, base, goe[0], 'weight') +
                             nx.shortest_path_length(G, goe[-1], base, 'weight'))
 
-print(goe_dict)
+# get weight ordered GoE list
+# each element of list is a dictionary (GoE, cost) that has the same number of edges
+ordered_dict_list = get_weight_orderd_goe_list(goe_dict)
 
-# build the graph by looping all goe_dict dictionary
-graph_of_states = nx.Graph()
-graph_of_states.add_node(0)
+print(ordered_dict_list)
 
-# for state in goe_dict:
-#     graph_of_states.add_node(state, weight = goe_dict[state])
+# build the optimal path
+optimal_path_dict_final = get_final_optimal_path(ordered_dict_list)
+
+# print optimal path dictionary raw data
+print(optimal_path_dict_final)
+
+# get actual path and calculate total cost
+# for key in optimal_path_dict_final:
 
 
 
-#graph_of_states.edges()
 
-print(graph_of_states)
+
 
 
 
